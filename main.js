f0d37c74ca0ea1dc4fc1beaf3b88728d0d9e7a57
Dwayne Levene <39275855+dwayneglevene@users.noreply.github.com>
2022-11-13 17:44:24 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    console.log(data);
+    createBreedList(data.message);
+}
+
+start();
+
+
+function createBreedList(breeList){
+
+}
